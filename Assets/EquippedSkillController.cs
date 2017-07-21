bf62eb51806afeee7a9ee434b4518bca0ccf2ec1
frecre <frecre@Edsels-MacBook-Pro.local>
2017-07-21 15:30:04 +0800

    Deck update

--- Assets/EquippedSkillController.cs
+++ Assets/EquippedSkillController.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class EquippedSkillController : MonoBehaviour {
 
 	private List<SkillModel> equipCardList = new List<SkillModel> ();
+	private List<GameObject> cardsObject = new List<GameObject>();
 	public UnlockedSkillController unlockedSkill;
 
 	// Use this for initialization
@@ -15,10 +16,40 @@ public class EquippedSkillController : MonoBehaviour {
 			if (this.transform.GetChild (i).childCount == 0) {
 				GameObject charCard = SystemResourceController.Instance.LoadPrefab ("CharCard", this.transform.GetChild (i).gameObject);
 				charCard.GetComponent<CharCardController> ().SetCardParameter (equipCardList [i]);
+				cardsObject.Add (charCard.gameObject);
+				charCard.name = "Skill" + i;
+				charCard.GetComponent<Button> ().onClick.AddListener (() => {
+					OnClickEquippedCard(charCard.gameObject);
+				});
 			}
 		}
 
 		unlockedSkill.ShowCharacterCards (equipCardList);
 		SkillManager.SetSkillEnqueue (equipCardList);
 	}
+
+	public void InitiateSwapping(){
+		ShakeSkillCards();
+	}
+
+	public void ShakeSkillCards(){
+		foreach (GameObject card in cardsObject) {
+			TweenFacade.TweenDoPunchRotation (card.transform,0.5f,new Vector3(0,0,1),10,1f);
+		}
+	}
+
+	public void OnClickEquippedCard(GameObject clickedCard){
+		TweenFacade.StopTweens ();
+		Debug.Log (clickedCard.name);
+
+		PartDeckController.Instance.unlockedSkillController.currectSelectedCharacterCard.transform.parent = 
+			clickedCard.transform.parent;
+		GameObject unlockedSkill = PartDeckController.Instance.unlockedSkillController.currectSelectedCharacterCard.gameObject;
+		unlockedSkill.GetComponent<Button>().onClick.AddListener (() => {
+			PartDeckController.Instance.unlockedSkillController.OnClickCharacterUse(unlockedSkill);
+		});
+		clickedCard.transform.parent = PartDeckController.Instance.unlockedSkillController.currentSelectedCardSlot.transform;
+		SkillManager.SetSkillEnqueue (equipCardList);
+	}
+
 }
--- Assets/UnlockedSkillController.cs
+++ Assets/UnlockedSkillController.cs
@@ -6,14 +6,14 @@ public class UnlockedSkillController : MonoBehaviour
 {
 	private List<SkillModel> charCardList = new List<SkillModel> ();
 	public Text charCardCount;
-
+	public GameObject unlockedSkillTitle;
+	public GameObject currentSelectedCardSlot;
+	public GameObject currectSelectedCharacterCard;
 	// Show unlocked cards, if cards already in equip, do not show
 	public void ShowCharacterCards (List<SkillModel> equipCardList)
 	{
 		charCardList = SkillManager.GetCSVSkillList ();
 		charCardCount.text = charCardList.Count.ToString () + "/20";
-
-
 		for (int i = 0; i < charCardList.Count; i++) {
 			bool hasCardInEquip = false;
 
@@ -29,6 +29,9 @@ public class UnlockedSkillController : MonoBehaviour
 					if (this.transform.GetChild (k).childCount == 0) {
 						GameObject charCard = SystemResourceController.Instance.LoadPrefab ("CharCard", this.transform.GetChild (k).gameObject);
 						charCard.GetComponent<CharCardController> ().SetCardParameter (charCardList[i]);
+						charCard.GetComponent<Button>	().onClick.AddListener (() => {
+							OnClickCharacterCard(charCard.GetComponent<Button>());
+						});
 						break;
 					}
 				}
@@ -37,4 +40,24 @@ public class UnlockedSkillController : MonoBehaviour
 		}
 			
 	}
+	public void OnClickCharacterCard(Button clickedCharacterCard){
+		currentSelectedCardSlot = clickedCharacterCard.transform.parent.gameObject;
+		currectSelectedCharacterCard = clickedCharacterCard.gameObject;
+		GameObject selectionPrefab = SystemResourceController.Instance.LoadPrefab ("SkillSelectPrefab", clickedCharacterCard.gameObject);
+		selectionPrefab.transform.SetAsFirstSibling ();
+		selectionPrefab.transform.position = clickedCharacterCard.transform.position;
+		GameObject useButton = selectionPrefab.transform.GetChild (2).gameObject;
+		useButton.GetComponent<Button> ().onClick.AddListener (() => {
+			OnClickCharacterUse(clickedCharacterCard.gameObject);
+		});
+		TweenFacade.TweenScaleToLarge (selectionPrefab.transform.parent.transform,Vector3.one,0.3f);
+	}
+
+	public void OnClickCharacterUse(GameObject characterUsed){
+		Destroy (characterUsed.transform.GetChild (0).gameObject);
+		characterUsed.transform.parent = unlockedSkillTitle.transform.parent;
+		TweenFacade.TweenMoveTo (characterUsed.transform,unlockedSkillTitle.transform.localPosition,0.3f);
+		PartDeckController.Instance.equippedSkillController.InitiateSwapping ();
+	}
+
 }
